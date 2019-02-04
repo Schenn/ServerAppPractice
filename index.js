14fe1598c1908e771c72e2105dc089b2be6c6c68
Steven Chennault <schenn@gmail.com>
2019-02-03 16:59:00 -0800

    Migrate route handling to RouteHandler

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 const env = require("./config");
-const Server = require("./server");
-const fs = require("fs");
+const Server = require("./lib/Server");
 
 const Router = require("./lib/Router");
 const Writer = require("./lib/Writer");
-
-const createServer = function(routers, secure=false){
-  let server = new Server(routers);
-  if(secure && fs.existsSync(env.https.keycert)){
-    server.createServer(true, {
-      'key': fs.readFileSync(env.https.key),
-      'cert': fs.readFileSync(env.https.cert)
-    });
-  } else {
-    server.createServer();
-  }
-
-  server.listen({
-    port: (secure) ? env.https.port : env.port,
-    env: env.env
-  });
-};
 
 let router = new Router();
 let writer = new Writer();
@@ -32,5 +14,6 @@ writer.readSync('routecache.json', true, (data)=>{
   router.addRoutes(data.httpsRoutes, true);
 });
 
-createServer(router.httpRoutes);
-createServer(router.httpsRoutes, true);
+let server = new Server(router, env.port);
+server.security = env.https;
+server.createConnections();
