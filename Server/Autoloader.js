005d1892fbf1252a99f34e51e84a08c86f95a7e9
Steven Chennault <schenn@gmail.com>
2019-07-26 18:08:32 -0700

    Cache namespaces as they come into the autoloader.

--- Server/Autoloader.js
+++ Server/Autoloader.js
@@ -1,11 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
+const cache = {};
+
 module.exports = function (namespace) {
   let filePath = path.join(process.cwd(), namespace.replace("\\", "/"))+".js";
-  if(fs.existsSync(filePath)){
-    return require(filePath);
+  let ref = null;
+  if(typeof cache[filePath] !== "undefined"){
+    ref = cache[filePath];
+  } else if(fs.existsSync(filePath)){
+    cache[filePath] = require(filePath);
+    ref = cache[filePath];
   } else {
     throw `No class found at path:${filePath}`;
   }
+
+  return ref;
 };
